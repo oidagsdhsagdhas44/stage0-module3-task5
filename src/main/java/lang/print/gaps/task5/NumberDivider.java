baf760f304b300c096b5f1c24976e871d5215ccf
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-02-19 01:47:24 +0400

    stage0 module3 task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
+//    Create a program that will perform simple division of numbers, you will be given a number and your
+//    task will be to create a program that divides a number by 5 and print correct result:
     void divide(int toBeDivided) {
+        float result = (float)toBeDivided / 5;
+        System.out.println(result);
     }
 }
